88d15fcf08ed13413ad941cfb7c5d71097830027
Michelangelo <garrys505@gmail.com>
2020-11-27 19:36:43 +0700

    Fixed again

--- Server/Server.py
+++ Server/Server.py
@@ -7,6 +7,7 @@ from profile.user import UserProfile
 
 class ChatServer:
     connections = []
+    nicknames = []
     message_kit = Message()
 
     def __init__(self, ip_address, port_server, number_listen):
@@ -21,52 +22,52 @@ class ChatServer:
 
     def get_messages(self, connection, ip_address, name):
         message_new_client = "Welcome to this chatroom!"
-        message_broadcast = f"<{ip_address}@{name}>подключился к чату"
         data_package = self.message_kit.get_package(message_new_client)
-        self.broadcast(message_broadcast)
         connection.send(data_package)
+        message_broadcast = f"<{ip_address}@{name}>connected to channel"
+        package_message = self.message_kit.get_package(message_broadcast)
+        self.broadcast(package_message)
         while True:
             data = self.message_kit.get_message(connection=connection)
             if data:
                 if data != 'close':
-                    print(f"<{ip_address}> {data} ")
+                    print(f"<{ip_address}@{name}> {data} ")
                     broadcast_message = f"<{ip_address}@{name}>" + data
                     data_package = self.message_kit.get_package(broadcast_message)
                     self.broadcast(message=data_package)
                 else:
                     data = f"<{ip_address}@{name}>покинул чат"
                     data_package = self.message_kit.get_package(data)
-                    self.close_connection(connection)
+                    self.close_connection(connection, name)
                     self.broadcast(message=data_package)
 
-    def close_connection(self, user):
-        if user in self.connections:
-            user.connection.close()
-            self.connections.remove(user)
-        length = len(self.connections)
-        if length == 0:
-            exit()
+    def close_connection(self, connection, nick):
+        if connection in self.connections:
+            connection.close()
+            self.connections.remove(connection)
+            self.nicknames.remove(nick)
+        print(self.nicknames)
 
     def broadcast(self, message):
-        for user in self.connections:
-            user.connection.send(message)
+        for connection in self.connections:
+            connection.send(message)
 
     def run(self):
         while True:
             connection, address = self.socket_server.accept()
-            user = UserProfile()
-            user.connection = connection
-            user.nickname = self.message_kit.get_message(connection=connection)
-            user.address = address[0]
-            self.connections.append(user)
-            print(f"<{user.address}@{user.nickname}> - connected")
+            connection = connection
+            nickname = self.message_kit.get_message(connection=connection)
+            self.connections.append(connection)
+            self.nicknames.append(nickname)
+            print(self.nicknames)
+            print(f"<{address[0]}@{nickname}> - connected")
             thread_messages = threading.Thread(target=self.get_messages,
-                                               args=(user.connection, user.address, user.nickname), daemon=True)
+                                               args=(connection, address, nickname), daemon=True)
             thread_messages.start()
 
 
 if __name__ == '__main__':
     ip = '127.0.0.1'
-    port = 9876
+    port = 9871
     server = ChatServer(ip_address=ip, port_server=port, number_listen=100)
     server.run()
